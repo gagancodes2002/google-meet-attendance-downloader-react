71a6344e6e110e6f0740d12c3d57342e8b1852e3
gagancodes2002 <77449656+gagancodes2002@users.noreply.github.com>
2021-09-04 00:09:23 +0530

    First Push
    basic react structure with redundant files

---
+++ src/components/Buttons/CustomButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+const CustomButtons = () => {
+  return (
+    <div>
+      <button class="ui icon button">
+        <i class="cloud icon"></i>
+      </button>
+    </div>
+  );
+};
+
+export default CustomButtons;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomButtons from './Buttons/CustomButtons';
+const HomePage = () => {
+	return (
+		<div>
+			<CustomButtons/>
+		</div>
+	);
+};
+
+export default HomePage;
